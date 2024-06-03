7cbf61c0fbf4c104f46b0601642874fab8567075
Tinodaishe Tembo <synthmancer@gmail.com>
2024-06-03 12:43:45 +0200

    add shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
 	return (
 		<main>
-			<h1>Home Page</h1>
+			<h1>Hello Mom!</h1>
+			<Button>Click Me</Button>
 		</main>
 	);
 }
